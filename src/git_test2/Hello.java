2f3f4e652cbf5af8d409aa563a89acf1fb66db11
Choi,jisung <chlwltjd001001@gmail.com>
2025-09-15 00:58:52 +0900

    화면 출력 방법 수정

--- src/git_test2/Hello.java
+++ src/git_test2/Hello.java
@@ -1,10 +1,7 @@
 package git_test2;
-
-public class Hello {
-	public void say() {
-		String message="안녕하세요";
-		public void say(){
-			System.out.println(message);
-		}
-	}
-}
+ public class Hello {
+	 String message = "안녕하세요";
+	  public String say() { 
+		return message;
+		 }
+		  }
